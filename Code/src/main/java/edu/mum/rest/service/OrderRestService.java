306abe27e3716d6b6d1a2a36614ed88ec9c2d85e
Thanh Tran <hoaithanhht3@gmail.com>
2019-03-20 11:46:49 -0500

    rest service

--- Code/src/main/java/edu/mum/rest/service/OrderRestService.java
+++ Code/src/main/java/edu/mum/rest/service/OrderRestService.java
@@ -1,5 +1,12 @@
 package edu.mum.rest.service;
 
+import java.util.List;
+
+import edu.mum.domain.Order;
+
+
 public interface OrderRestService {
 
+	public List<Order> findAll();
+	
 }
--- Code/src/main/java/edu/mum/rest/service/impl/OrderRestServiceImpl.java
+++ Code/src/main/java/edu/mum/rest/service/impl/OrderRestServiceImpl.java
@@ -1,7 +1,26 @@
 package edu.mum.rest.service.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import edu.mum.domain.Order;
+import edu.mum.rest.RestHttpHeader;
 import edu.mum.rest.service.OrderRestService;
 
+@Service
 public class OrderRestServiceImpl implements OrderRestService {
-
+	
+	@Autowired
+	RestHttpHeader restHttpHeader;
+	
+	public List<Order> findAll(){
+		RestTemplate restTemplate = restHttpHeader.getRestTemplate();
+		return Arrays.asList(restTemplate.exchange("http://localhost:8080/SpringArrival_RestService/orders", HttpMethod.GET, restHttpHeader.getHttpEntity(), Order[].class).getBody());
+	
+	}
 }
