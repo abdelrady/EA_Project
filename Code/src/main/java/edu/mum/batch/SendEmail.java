03242f9a2230ffab45c32b8b5dc029f3aaca170a
Thanh Tran <hoaithanhht3@gmail.com>
2019-03-22 23:13:00 -0500

    move receiver to config file

--- Code/src/main/java/edu/mum/batch/SendEmail.java
+++ Code/src/main/java/edu/mum/batch/SendEmail.java
@@ -5,16 +5,23 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import edu.mum.service.impl.EmailService;
 
 public class SendEmail implements Tasklet {
 	@Autowired
 	EmailService emailService;
+	
+	@Value("${email.receiver.name}")
+	private String name;
+	@Value("${email.receiver.username}")
+	private String username;
+	
 	@Override
  	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
  		// TODO Auto-generated method stub
-		emailService.sendResultNotificationMail("Thanh Tran", "hoaithanhht3@gmail.com", chunkContext);
+		emailService.sendResultNotificationMail(name,username , chunkContext);
  		return RepeatStatus.FINISHED;
  	}
 }
